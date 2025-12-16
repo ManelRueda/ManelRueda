e5c8792e7e922704f4c559c0e5ea2f7ebe689ddc
XavierRV2006 <xavier.rodriguez.7e8@itb.cat>
2025-12-16 19:38:40 +0100

    Teclado mejorado

--- Ejercicios .js/tecladoNumerico.js
+++ Ejercicios .js/tecladoNumerico.js
@@ -1,21 +1,81 @@
 
 
+let palabra = "";
+function palabraSecreta() {
+   fetch('https://random-word-api.herokuapp.com/word?lang=es&length=5')
+       .then(response => response.json())
+       .then(data => {
+           palabra = data[0]; // La API devuelve un array, ej: ["perro"]
+          
+           palabra=palabra.toUpperCase();
+           console.log("Tu palabra secreta es:", palabra);
+
+       });
+
+}
+
+
 function crearTeclado() {
     //Cojo el div del teclado, para luego ir añadiendo
     let teclado = document.getElementById("teclado");
-    
+
     for (let i = 65; i < 91; i++) {
         //Creo la tecla
         let tecla = document.createElement("div");
         tecla.innerHTML = "<p>" + String.fromCharCode(i) + "</p>";
         tecla.className = "tecla";
 
-        tecla = cambiarFondoTecla(i,tecla);
+        tecla = cambiarFondoTecla(i, tecla);
+
+        tecla.setAttribute("onclick", "escribeTecla('" + String.fromCharCode(i) + "')");
+
+        //Añado la tecla al teclado
+        teclado.appendChild(tecla);
+
+
+    }
+
+    for (let i = 0; i < 10; i++) {
+        //Creo la tecla
+        let tecla = document.createElement("div");
+        tecla.innerHTML = "<p>" + i + "</p>";
+        tecla.className = "tecla";
+
+        tecla = cambiarFondoTecla(i, tecla);
+
+        tecla.setAttribute("onclick", "escribeTecla('" + i + "')");
 
         //Añado la tecla al teclado
         teclado.appendChild(tecla);
     }
-    
+
+    let borrar = document.querySelector(".borrarLettra");
+    if (borrar) {
+        borrar.addEventListener("click", borrarLetra);
+    }
+
+    palabraSecreta();
+}
+
+function escribeTecla(letra) {
+    console.log(letra);
+    let miTexto = document.getElementById("miTexto");
+
+    if (miTexto.textContent.length < 5) {
+        miTexto.textContent += letra;
+        
+    }
+    else {
+        miTexto.style.color = "red";
+    }
+}
+
+function borrarLetra() {
+    let miTexto = document.getElementById("miTexto");
+    if (miTexto.textContent.length > 0) {
+        miTexto.textContent = miTexto.textContent.substring(0, miTexto.textContent.length - 1);
+        miTexto.style.color = "black";
+    }
 }
 
 function esPrimo(num) {
